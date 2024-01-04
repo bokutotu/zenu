8d1604271016fbca0b03b6c53b4fbf34dd5ad137
bokutotu <mushin.hudoushin@gmail.com>
2024-01-05 00:40:37 +0900

    remove graph

--- ruml-autograd/src/lib.rs
+++ ruml-autograd/src/lib.rs
@@ -1,7 +1,18 @@
+use ruml_graph::{Graph, Node};
+
 pub trait Layer {
-    type Forward;
-    type Backward;
+    type Forward: Node;
+    type Backward: Node;
 
     fn forward_node(&self) -> Self::Forward;
     fn backward_node(&self) -> Self::Backward;
+}
+
+pub trait AutoGradGraph: Graph + Node {
+    fn forward(&self);
+    fn backward(&self);
+}
+
+pub struct Sequential {
+    layers: Vec<Box<dyn Node>>,
 }
