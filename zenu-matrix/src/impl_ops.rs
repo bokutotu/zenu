ea2babea3d7fceeb832898ec17ad8cfc340a72dd
bokutotu <mushin.hudoushin@gmail.com>
2024-03-07 06:23:13 +0900

    update ops

--- zenu-matrix/src/impl_ops.rs
+++ zenu-matrix/src/impl_ops.rs
@@ -4,133 +4,44 @@ use crate::{
     dim::DimTrait,
     matrix::ToOwnedMatrix,
     matrix_impl::Matrix,
-    memory::{ToOwnedMemory, ToViewMemory, View},
-    memory_impl::{OwnedMem, ViewMem},
+    memory::{ToOwnedMemory, ToViewMemory},
     num::Num,
-    operation::basic_operations::{MatrixAdd, MatrixDivAssign, MatrixMul, MatrixSub},
+    operation::basic_operations::{MatrixAdd, MatrixDiv, MatrixMul, MatrixSub},
 };
 
-impl<'a, D, T> Add<T> for Matrix<ViewMem<'a, T>, D>
-where
-    D: DimTrait,
-    T: Num,
-{
-    type Output = Matrix<OwnedMem<T>, D>;
+macro_rules! impl_ops {
+    ($trait:ident, $method:ident, $use_trait:ident, $use_trait_method:ident) => {
+        impl<T: Num, M: ToViewMemory<Item = T> + ToOwnedMemory, D: DimTrait> $trait<T>
+            for Matrix<M, D>
+        {
+            type Output = Matrix<M::Owned, D>;
 
-    fn add(self, rhs: T) -> Self::Output {
-        let mut owned = ToOwnedMatrix::to_owned(&self);
-        MatrixAdd::add(&mut owned, self, rhs);
-        owned
-    }
+            fn $method(self, rhs: T) -> Self::Output {
+                let mut owned = ToOwnedMatrix::to_owned(&self);
+                $use_trait_method::$method(&mut owned, self, rhs);
+                owned
+            }
+        }
+
+        impl<
+                T: Num,
+                M1: ToViewMemory<Item = T>,
+                M2: ToOwnedMemory<Item = T> + ToViewMemory<Item = T>,
+                D1: DimTrait,
+                D2: DimTrait,
+            > $trait<Matrix<M1, D1>> for Matrix<M2, D2>
+        {
+            type Output = Matrix<M2::Owned, D2>;
+
+            fn $method(self, rhs: Matrix<M1, D1>) -> Self::Output {
+                let mut owned = ToOwnedMatrix::to_owned(&self);
+                $use_trait_method::$method(&mut owned, self, rhs);
+                owned
+            }
+        }
+    };
 }
-
-impl<D1, D2, M1, M2, T> Add<Matrix<M1, D1>> for Matrix<M2, D2>
-where
-    D1: DimTrait,
-    D2: DimTrait,
-    M1: ToViewMemory<Item = T>,
-    M2: ToViewMemory<Item = T> + ToOwnedMemory,
-    T: Num,
-{
-    type Output = Matrix<M2::Owned, D2>;
-
-    fn add(self, rhs: Matrix<M1, D1>) -> Self::Output {
-        let mut owned = ToOwnedMatrix::to_owned(&self);
-        MatrixAdd::add(&mut owned, self, rhs);
-        owned
-    }
-}
-
-impl<M, D, T> Mul<T> for Matrix<M, D>
-where
-    M: View<Item = T>,
-    D: DimTrait,
-    T: Num,
-{
-    type Output = Matrix<M::Owned, D>;
-
-    fn mul(self, rhs: T) -> Self::Output {
-        let mut owned = ToOwnedMatrix::to_owned(&self);
-        MatrixMul::mul(&mut owned, self, rhs);
-        owned
-    }
-}
-
-impl<'a, 'b, D1, D2, T> Mul<Matrix<ViewMem<'a, T>, D1>> for Matrix<ViewMem<'b, T>, D2>
-where
-    D1: DimTrait,
-    D2: DimTrait,
-    T: Num,
-{
-    type Output = Matrix<OwnedMem<T>, D2>;
-
-    fn mul(self, rhs: Matrix<ViewMem<T>, D1>) -> Self::Output {
-        let mut owned = ToOwnedMatrix::to_owned(&self);
-        MatrixMul::mul(&mut owned, self, rhs);
-        owned
-    }
-}
-
-impl<T, D, M> Sub<T> for Matrix<M, D>
-where
-    M: ToViewMemory<Item = T> + ToOwnedMemory,
-    D: DimTrait,
-    T: Num,
-{
-    type Output = Matrix<M::Owned, D>;
-
-    fn sub(self, rhs: T) -> Self::Output {
-        let mut owned = ToOwnedMatrix::to_owned(&self);
-        MatrixSub::sub(&mut owned, self, rhs);
-        owned
-    }
-}
-
-impl<T, D1, D2, M1, M2> Sub<Matrix<M1, D1>> for Matrix<M2, D2>
-where
-    T: Num,
-    D1: DimTrait,
-    D2: DimTrait,
-    M1: ToViewMemory<Item = T>,
-    M2: ToViewMemory<Item = T> + ToOwnedMemory,
-{
-    type Output = Matrix<M2::Owned, D2>;
-
-    fn sub(self, rhs: Matrix<M1, D1>) -> Self::Output {
-        let mut owned = ToOwnedMatrix::to_owned(&self);
-        MatrixSub::sub(&mut owned, self, rhs);
-        owned
-    }
-}
-
-impl<M1, M2, D1, D2, T> Div<Matrix<M1, D1>> for Matrix<M2, D2>
-where
-    D1: DimTrait,
-    D2: DimTrait,
-    M1: ToViewMemory<Item = T>,
-    M2: ToViewMemory<Item = T> + ToOwnedMemory,
-    T: Num,
-{
-    type Output = Matrix<M2::Owned, D2>;
-
-    fn div(self, rhs: Matrix<M1, D1>) -> Self::Output {
-        let mut owned = ToOwnedMatrix::to_owned(&self);
-        MatrixDivAssign::div_assign(&mut owned, rhs);
-        owned
-    }
-}
-
-impl<M, D, T> Div<T> for Matrix<M, D>
-where
-    M: ToViewMemory<Item = T> + ToOwnedMemory,
-    D: DimTrait,
-    T: Num,
-{
-    type Output = Matrix<M::Owned, D>;
-
-    fn div(self, rhs: T) -> Self::Output {
-        let mut owned = ToOwnedMatrix::to_owned(&self);
-        MatrixDivAssign::div_assign(&mut owned, rhs);
-        owned
-    }
-}
+impl_ops!(Add, add, MatrixAdd, MatrixAdd);
+impl_ops!(Mul, mul, MatrixMul, MatrixMul);
+impl_ops!(Sub, sub, MatrixSub, MatrixSub);
+impl_ops!(Div, div, MatrixDiv, MatrixDiv);
