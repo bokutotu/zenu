b3feb8804639eb4f40cacbcb9e1d4ec6960cd726
bokutotu <mushin.hudoushin@gmail.com>
2024-03-16 17:11:18 +0900

    feat(layer): add Layer trait and dependencies in Cargo.toml
    This commit introduces the Layer trait in lib.rs, which is expected to
    be implemented by different types of layers in a neural network. It
    also adds dependencies for zenu-matrix and zenu-autograd in Cargo.toml.
    The Layer trait includes methods for forward propagation, initializing
    parameters, and retrieving parameters.

--- zenu-layer/src/lib.rs
+++ zenu-layer/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use zenu_autograd::Variable;
+use zenu_matrix::num::Num;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub trait Layer<T: Num> {
+    fn forward(&self, input: Variable<T>) -> Variable<T>;
+    fn init_parameters(&self);
+    fn parameters(&self) -> Vec<Variable<T>>;
 }
