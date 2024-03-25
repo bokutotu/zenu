ebd16a632102c415d69e76f635055f409acab29d
bokutotu <mushin.hudoushin@gmail.com>
2024-03-26 04:54:58 +0900

    This commit implements the deconv2d_inner function in the deconv2d_impl module. This function is essential for performing the deconvolution operation in convolutional neural networks.
    The commit also includes a test for the deconv2d_inner function to
    ensure its correct operation. The test uses a 3x5x5 image and a 3x3
    kernel with a stride of 1x1 and padding of 1x1.
    
    BREAKING CHANGE: The deconv2d_inner function is now available for use
    in the deconv2d_impl module.

--- zenu-autograd/src/functions/conv2d/col2im.rs
+++ zenu-autograd/src/functions/conv2d/col2im.rs
@@ -1,7 +1,7 @@
 use zenu_matrix::{
     constructor::zeros::Zeros,
     dim::DimDyn,
-    matrix::{MatrixSliceDyn, MatrixSliceMutDyn, ToOwnedMatrix},
+    matrix::{MatrixBase, MatrixSliceDyn, MatrixSliceMutDyn, ToOwnedMatrix},
     matrix_impl::{Matrix, OwnedMatrixDyn},
     memory_impl::{OwnedMem, ViewMem},
     num::Num,
