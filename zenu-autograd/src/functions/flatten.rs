78e7013c9f55a5c5068169a2385cab33f8b2fcd2
bokutotu <mushin.hudoushin@gmail.com>
2024-04-01 18:33:55 +0900

    feat(flatten and reshape): Add flatten and reshape functions
    This commit introduces two new functions, flatten and reshape, to the
    autograd library. These functions are essential for reshaping the input
    and output data in neural networks. The flatten function reshapes the
    input into a 1D array, while the reshape function allows for more
    general reshaping of the data.

---
+++ zenu-autograd/src/functions/flatten.rs
@@ -0,0 +1,58 @@
+use std::{cell::RefCell, rc::Rc};
+
+use zenu_matrix::{
+    dim::DimTrait,
+    matrix::MatrixBase,
+    num::Num,
+    operation::{copy_from::CopyFrom, reshape::Reshape},
+};
+
+use crate::{creator::zeros::zeros, Function, Variable, VariableWeak};
+
+use super::reshape::reshape;
+
+struct Flatten<T: Num> {
+    input: Variable<T>,
+    output: VariableWeak<T>,
+}
+
+impl<T: Num> Flatten<T> {
+    fn new(input: Variable<T>, output: Variable<T>) -> Self {
+        let output = output.downgrade();
+        Self { input, output }
+    }
+}
+
+impl<T: Num> Function<T> for Flatten<T> {
+    fn forward(&self) {
+        let output_shape = self.output.upgrade().unwrap().get_data().shape();
+        let input_mat = self.input.get_data();
+        self.output
+            .upgrade()
+            .unwrap()
+            .get_data_mut()
+            .copy_from(&input_mat.reshape(output_shape.slice()));
+    }
+
+    fn backward(&self) {
+        let output_grad = self.output.upgrade().unwrap().get_grad().unwrap();
+        self.input
+            .set_grad(reshape(output_grad, self.input.get_data().shape().slice()));
+    }
+
+    fn get_inputs(&self) -> Vec<Variable<T>> {
+        vec![self.input.clone()]
+    }
+}
+
+pub fn flatten<T: Num>(input: Variable<T>) -> Variable<T> {
+    let input_shape = input.get_data().shape();
+    let batch_size = input_shape[0];
+    let num_elm = input_shape.num_elm();
+    let output_shape = [batch_size, num_elm / batch_size];
+    let output = zeros(&output_shape);
+    let flatten = Flatten::new(input, output.clone());
+    flatten.forward();
+    output.set_creator(Rc::new(RefCell::new(Box::new(flatten))));
+    output
+}
---
+++ zenu-autograd/src/functions/reshape.rs
@@ -0,0 +1,55 @@
+use std::{cell::RefCell, rc::Rc};
+
+use zenu_matrix::{
+    dim::DimTrait,
+    matrix::{MatrixBase, ToViewMutMatrix},
+    num::Num,
+    operation::{copy_from::CopyFrom, reshape::Reshape as MatrixReshape},
+};
+
+use crate::{creator::zeros::zeros, Function, Variable, VariableWeak};
+
+struct Reshape<T: Num> {
+    input: Variable<T>,
+    output: VariableWeak<T>,
+}
+
+impl<T: Num> Reshape<T> {
+    fn new(input: Variable<T>, output: Variable<T>) -> Self {
+        let output = output.downgrade();
+        Self { input, output }
+    }
+}
+
+impl<T: Num> Function<T> for Reshape<T> {
+    fn forward(&self) {
+        let output_shape = self.output.upgrade().unwrap().get_data().shape();
+        let input_mat = self.input.get_data();
+        self.output
+            .upgrade()
+            .unwrap()
+            .get_data_mut()
+            .to_view_mut()
+            .copy_from(&input_mat.reshape(output_shape.slice()));
+    }
+
+    fn backward(&self) {
+        let output_grad = self.output.upgrade().unwrap().get_grad().unwrap();
+        self.input.set_grad(reshape(
+            output_grad.clone(),
+            self.input.get_data().shape().slice(),
+        ));
+    }
+
+    fn get_inputs(&self) -> Vec<Variable<T>> {
+        vec![self.input.clone()]
+    }
+}
+
+pub fn reshape<T: Num>(input: Variable<T>, output_shape: &[usize]) -> Variable<T> {
+    let output = zeros(output_shape);
+    let reshape = Reshape::new(input, output.clone());
+    reshape.forward();
+    output.set_creator(Rc::new(RefCell::new(Box::new(reshape))));
+    output
+}
