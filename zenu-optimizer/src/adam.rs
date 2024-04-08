3f8cc93e643a7b41dcefd3500d14830f19e6faf8
bokutotu <mushin.hudoushin@gmail.com>
2024-04-08 20:14:35 +0900

    feat(adam.rs): refactor Adam optimizer and add unit tests
    The Adam optimizer in adam.rs has been refactored for better
    readability and maintainability. The variable names have been
    changed to be more descriptive and the update functions have
    been modified for better performance. Unit tests have also been
    added to ensure the correctness of the optimizer.

--- zenu-optimizer/src/adam.rs
+++ zenu-optimizer/src/adam.rs
@@ -7,7 +7,7 @@ use zenu_matrix::{
     matrix_impl::OwnedMatrixDyn,
     num::Num,
     operation::{
-        basic_operations::{MatrixPowf, MatrixSubAssign},
+        basic_operations::{MatrixAddAssign, MatrixPowf, MatrixSqrt, MatrixSubAssign},
         copy_from::CopyFrom,
     },
 };
@@ -16,11 +16,11 @@ use crate::Optimizer;
 
 pub struct Adam<T: Num> {
     pub alpha: T,
-    pub beta1: T,
-    pub beta2: T,
-    pub epsilon: T,
-    pub v_t_1: Rc<RefCell<Vec<OwnedMatrixDyn<T>>>>,
-    pub s_t_1: Rc<RefCell<Vec<OwnedMatrixDyn<T>>>>,
+    beta1: T,
+    beta2: T,
+    epsilon: T,
+    m: Rc<RefCell<Vec<OwnedMatrixDyn<T>>>>,
+    v: Rc<RefCell<Vec<OwnedMatrixDyn<T>>>>,
 }
 
 impl<T: Num> Adam<T> {
@@ -30,78 +30,154 @@ impl<T: Num> Adam<T> {
             beta1,
             beta2,
             epsilon,
-            v_t_1: Rc::new(RefCell::new(Vec::new())),
-            s_t_1: Rc::new(RefCell::new(Vec::new())),
+            m: Rc::new(RefCell::new(Vec::new())),
+            v: Rc::new(RefCell::new(Vec::new())),
         }
     }
 
-    fn init_v_t_1(&self, parameters: &[Variable<T>]) {
-        // zero vvectorで埋める
-        let v_t_1 = &mut *self.v_t_1.borrow_mut();
-        v_t_1.clear();
+    fn init_m(&self, parameters: &[Variable<T>]) {
+        let m = &mut *self.m.borrow_mut();
+        m.clear();
         for p in parameters {
-            v_t_1.push(OwnedMatrixDyn::zeros(p.get_data().shape()));
+            m.push(OwnedMatrixDyn::zeros(p.get_data().shape()));
         }
     }
 
-    fn init_s_t_1(&self, parameters: &[Variable<T>]) {
-        // zero vvectorで埋める
-        let s_t_1 = &mut *self.s_t_1.borrow_mut();
-        s_t_1.clear();
+    fn init_v(&self, parameters: &[Variable<T>]) {
+        let v = &mut *self.v.borrow_mut();
+        v.clear();
         for p in parameters {
-            s_t_1.push(OwnedMatrixDyn::zeros(p.get_data().shape()));
+            v.push(OwnedMatrixDyn::zeros(p.get_data().shape()));
         }
     }
 
-    fn update_v_t_1(&self, parameters: &[Variable<T>]) {
-        let v_t_1 = &mut *self.v_t_1.borrow_mut();
-        for (v, p) in v_t_1.iter_mut().zip(parameters) {
-            let v_clone = v.to_owned_matrix();
-            let mut v_mut = v.to_view_mut();
-            v_mut.copy_from(
-                &(v_clone * self.beta1
-                    + p.get_grad().unwrap().get_data() * (T::one() - self.beta1)),
-            );
+    fn update_m(&self, parameters: &[Variable<T>]) {
+        let m = &mut *self.m.borrow_mut();
+        for (m, p) in m.iter_mut().zip(parameters) {
+            let mut p_g = p.get_grad().unwrap().get_data();
+            let m_c = m.to_owned_matrix();
+            p_g.sub_assign(m_c);
+            m.to_view_mut().add_assign(p_g * (T::one() - self.beta1));
         }
     }
 
-    fn update_s_t_1(&self, parameters: &[Variable<T>]) {
-        let s_t_1 = &mut *self.s_t_1.borrow_mut();
-        for (s, p) in s_t_1.iter_mut().zip(parameters) {
-            let s_clone = s.to_owned_matrix();
-            let mut s_mut = s.to_view_mut();
-            let mut p_cloned = p.get_data().to_owned();
-            p_cloned
-                .to_view_mut()
-                .powf(p.get_data().to_view(), T::from_usize(2));
-            s_mut.copy_from(&(s_clone * self.beta2 + p_cloned * (T::one() - self.beta2)));
+    fn update_v(&self, parameters: &[Variable<T>]) {
+        let v = &mut *self.v.borrow_mut();
+        for (v, p) in v.iter_mut().zip(parameters) {
+            let p_g = p.get_grad().unwrap().get_data();
+            let v_c = v.to_owned_matrix();
+            let mut p_g = p_g.to_view() * p_g.to_view();
+            p_g.sub_assign(v_c);
+            v.to_view_mut().add_assign(p_g * (T::one() - self.beta2));
         }
     }
 
     fn update_parameters(&self, parameters: &[Variable<T>]) {
-        let v_t_1 = self.v_t_1.borrow();
-        let s_t_1 = self.s_t_1.borrow();
-        for ((p, v), s) in parameters.iter().zip(v_t_1.iter()).zip(s_t_1.iter()) {
-            let v_hat = v.clone() / (T::one() - self.beta1);
-            let s_hat = s.clone() / (T::one() - self.beta2);
+        let m = self.m.borrow();
+        let v = self.v.borrow();
+        for ((p, m), v) in parameters.iter().zip(m.iter()).zip(v.iter()) {
+            let m_hat = m.clone() / (T::one() - self.beta1);
+            let mut v_hat = v.clone() / (T::one() - self.beta2);
+            let v_hat_c = v_hat.to_owned_matrix();
+            v_hat.to_view_mut().sqrt(v_hat_c);
 
-            let diff_p = v_hat * self.beta2 / (s_hat * self.epsilon);
-            p.get_data_mut().to_view_mut().sub_assign(diff_p);
+            let diff_p = m_hat * self.beta2 / (v_hat + self.epsilon);
+            p.get_data_mut()
+                .to_view_mut()
+                .sub_assign(diff_p * self.alpha);
         }
     }
 }
 
 impl<T: Num> Optimizer<T> for Adam<T> {
     fn update(&self, parameters: &[Variable<T>]) {
-        if self.v_t_1.borrow().is_empty() {
-            self.init_v_t_1(parameters);
+        if self.m.borrow().is_empty() {
+            self.init_m(parameters);
         }
-        if self.s_t_1.borrow().is_empty() {
-            self.init_s_t_1(parameters);
+        if self.v.borrow().is_empty() {
+            self.init_v(parameters);
         }
 
+        self.update_m(parameters);
+        self.update_v(parameters);
         self.update_parameters(parameters);
-        self.update_v_t_1(parameters);
-        self.update_s_t_1(parameters);
+    }
+}
+
+#[cfg(test)]
+mod adam {
+    use zenu_autograd::{
+        creator::from_vec::from_vec,
+        functions::{loss::mse::mean_squared_error, matmul::matmul},
+        Variable,
+    };
+    use zenu_matrix::{matrix::OwnedMatrix, matrix_impl::OwnedMatrixDyn, operation::asum::Asum};
+
+    use crate::Optimizer;
+
+    use super::Adam;
+
+    fn simple_function(
+        x: Variable<f64>,
+        weight1: Variable<f64>,
+        weight2: Variable<f64>,
+    ) -> Variable<f64> {
+        let x = matmul(x, weight1);
+        matmul(x, weight2)
+    }
+
+    fn adam_apply(
+        adam: &Adam<f64>,
+        forward_func: fn(Variable<f64>, Variable<f64>, Variable<f64>) -> Variable<f64>,
+        input: Variable<f64>,
+        target: Variable<f64>,
+        weight1: Variable<f64>,
+        weight2: Variable<f64>,
+    ) {
+        let output = forward_func(input.clone(), weight1.clone(), weight2.clone());
+        let loss = mean_squared_error(target, output);
+        loss.backward();
+        adam.update(&[weight1.clone(), weight2.clone()]);
+        loss.clear_grad();
+    }
+
+    #[test]
+    fn small_3_times() {
+        let adam = Adam::new(0.01, 0.9, 0.999, 1e-8);
+        let weight1 = from_vec(vec![1., 2., 3., 4.], [2, 2]);
+        let weight2 = from_vec(vec![1., 2.], [2, 1]);
+
+        let input = from_vec(vec![1., 2.], [1, 2]);
+        let target = from_vec(vec![1.], [1, 1]);
+
+        adam_apply(
+            &adam,
+            simple_function,
+            input,
+            target,
+            weight1.clone(),
+            weight2.clone(),
+        );
+
+        let weight_1_ans = OwnedMatrixDyn::from_vec(vec![0.9900, 1.9900, 2.99, 3.99], [2, 2]);
+        let weight_2_ans = OwnedMatrixDyn::from_vec(vec![0.990, 1.990], [2, 1]);
+        assert!((weight1.get_data() - weight_1_ans).asum() < 1e-2);
+        assert!((weight2.get_data() - weight_2_ans).asum() < 1e-2);
+
+        let input = from_vec(vec![1., 2.], [1, 2]);
+        let target = from_vec(vec![1.], [1, 1]);
+        adam_apply(
+            &adam,
+            simple_function,
+            input.clone(),
+            target.clone(),
+            weight1.clone(),
+            weight2.clone(),
+        );
+
+        let weight_1_ans = OwnedMatrixDyn::from_vec(vec![0.98, 1.98, 2.98, 3.98], [2, 2]);
+        let weight_2_ans = OwnedMatrixDyn::from_vec(vec![0.98, 1.98], [2, 1]);
+        assert!((weight1.get_data() - weight_1_ans).asum() < 1e-1);
+        assert!((weight2.get_data() - weight_2_ans).asum() < 1e-1);
     }
 }
