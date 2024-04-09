895f954b676a8b4efaa525b40a8a3d5fe064f9d5
bokutotu <mushin.hudoushin@gmail.com>
2024-04-09 18:18:01 +0900

    add cuda

--- zenu-matrix/benches/transpose_reshape_im2col.rs
+++ zenu-matrix/benches/transpose_reshape_im2col.rs
@@ -9,8 +9,6 @@ use zenu_matrix::{
 };
 
 fn transpose_reshape(a: Matrix<ViewMem<f32>, DimDyn>) {
-    // let a = a.transepose_by_index(&[1, 2, 3, 0, 4, 5]);
-    // let _ = a.reshape_new_matrix([16 * 3 * 3, 32 * 126 * 126]);
     let a = a.reshape([32, 3 * 3 * 16, 126 * 126]);
     let a = a.transpose_by_index_inplace(&[1, 0, 2]);
     let _ = a.reshape([16 * 3 * 3, 32 * 126 * 126]);
