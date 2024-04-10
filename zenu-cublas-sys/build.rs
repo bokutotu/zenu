4b23743b6ec2f2b5ff9d318b13cc79f6ee8828c7
bokutotu <mushin.hudoushin@gmail.com>
2024-04-10 16:05:07 +0900

    feat: Enhance CUDA bindings and add Cublas support
    This commit enhances the CUDA bindings by uncommenting some lines in the
    build.rs files of zenu-cublas-sys and zenu-cuda-driver-sys. It also adds
    support for Cublas by creating a new ZenuCudaContext struct and a
    cublas_copy function in lib.rs. A new dependency, once_cell, is added to
    Cargo.toml. Some unnecessary lines are removed from Cargo.toml and
    build.rs files.

--- zenu-cublas-sys/build.rs
+++ zenu-cublas-sys/build.rs
@@ -18,9 +18,8 @@ fn main() {
         .clang_arg("/usr/local/cuda/include".to_string())
         .header("wrapper.h")
         .rustified_non_exhaustive_enum("cublas[A-Za-z]+_t")
-        // .rustified_non_exhaustive_enum("cuda.*")
+        .rustified_non_exhaustive_enum("cuda.*")
         .default_alias_style(bindgen::AliasVariation::TypeAlias)
-        // .rustfmt_bindings(true)
         .allowlist_type("^cublas.*")
         .allowlist_type("cublas.*")
         .allowlist_function("^cublas.*")
@@ -30,7 +29,6 @@ fn main() {
         .derive_eq(true)
         .derive_hash(true)
         .derive_ord(true)
-        // .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
 
