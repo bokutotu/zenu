b6cd2f86f0d6c6ca2fd6bde7308dfbb2dfee1d6c
bokutotu <mushin.hudoushin@gmail.com>
2024-04-17 02:03:18 +0900

    feat(kernel): Add CUDA kernel support
    This commit introduces CUDA kernel support to the project. It includes
    the addition of new CUDA files, modification of Cargo.toml files, and
    updates to the existing Rust files. The CUDA kernel support will enhance
    the project's ability to handle GPU computations efficiently.

---
+++ zenu-cuda-kernel-sys/kernel/kernel.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+
+#include "array_scalar.h"
+#include "element_wise.h"
+
+#ifdef __cplusplus
+}
+#endif
--- zenu-cuda/src/lib.rs
+++ zenu-cuda/src/lib.rs
@@ -1,11 +1,12 @@
 pub mod cublas;
+pub mod kernel;
 pub mod runtime;
 
 use std::{ptr::NonNull, sync::Mutex};
 
 use cublas::cublas_error::ZenuCublasError;
 use once_cell::sync::Lazy;
-use zenu_cublas_sys::{cublasContext, cublasCreate_v2};
+use zenu_cublas_sys::{cublasContext, cublasCreate_v2, cublasDestroy_v2};
 
 static ZENU_CUDA_STATE: Lazy<Mutex<ZenuCudaState>> = Lazy::new(|| Mutex::new(ZenuCudaState::new()));
 
@@ -36,5 +37,13 @@ impl ZenuCudaState {
 
     pub fn get_cublas(&self) -> NonNull<cublasContext> {
         self.cublas
+    }
+}
+
+impl Drop for ZenuCudaState {
+    fn drop(&mut self) {
+        unsafe {
+            cublasDestroy_v2(self.cublas.as_ptr());
+        }
     }
 }
