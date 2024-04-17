d5f61189208c0c47abaff9f48f98014c66c4df95
bokutotu <mushin.hudoushin@gmail.com>
2024-04-17 10:36:43 +0900

    test(kernel): Refactor array scalar tests with macros
    This commit refactors the array scalar tests in the kernel module. It
    introduces a macro to reduce code duplication and improve readability.
    The macro is used to generate tests for different operations (add, sub,
    mul, div) and data types (f32, f64). This change will make it easier to
    add new tests in the future.

--- zenu-cuda/src/kernel/mod.rs
+++ zenu-cuda/src/kernel/mod.rs
@@ -46,30 +46,106 @@ impl_array_scalar!(
 mod array_scalar {
     use crate::runtime::{cuda_copy, cuda_malloc, ZenuCudaMemCopyKind};
 
-    use super::array_scalar_add;
+    use super::*;
 
-    #[test]
-    fn add_scalar_f32() {
-        let a: Vec<f32> = vec![1.0, 2.0, 3.0, 4.0];
-        let mut out: Vec<f32> = vec![0.0; 4];
-        let scalar = 2.0;
-        let a_gpu: *mut f32 = cuda_malloc(a.len()).unwrap();
-        cuda_copy(
-            a_gpu,
-            a.as_ptr(),
-            a.len(),
-            ZenuCudaMemCopyKind::HostToDevice,
-        )
-        .unwrap();
-        let out_gpu: *mut f32 = cuda_malloc(out.len()).unwrap();
-        array_scalar_add(a_gpu, a.len(), 1, scalar, out_gpu);
-        cuda_copy(
-            out.as_mut_ptr(),
-            out_gpu,
-            out.len(),
-            ZenuCudaMemCopyKind::DeviceToHost,
-        )
-        .unwrap();
-        assert_eq!(out, vec![3.0, 4.0, 5.0, 6.0]);
+    macro_rules! impl_array_scalr_test {
+        ($test_name:ident, $input:expr, $scalar:expr, $ans:expr, $ty:ty, $kernel_func:ident) => {
+            #[test]
+            fn $test_name() {
+                let a: Vec<$ty> = $input;
+                let mut out = vec![0 as $ty; a.len()];
+                let scalar: $ty = $scalar;
+                let a_gpu = cuda_malloc(a.len()).unwrap();
+                cuda_copy(
+                    a_gpu,
+                    a.as_ptr(),
+                    a.len(),
+                    ZenuCudaMemCopyKind::HostToDevice,
+                )
+                .unwrap();
+                let out_gpu = cuda_malloc(out.len()).unwrap();
+                $kernel_func(a_gpu, a.len(), 1, scalar, out_gpu);
+                cuda_copy(
+                    out.as_mut_ptr(),
+                    out_gpu,
+                    out.len(),
+                    ZenuCudaMemCopyKind::DeviceToHost,
+                )
+                .unwrap();
+                let ans: Vec<$ty> = $ans;
+                assert_eq!(out, ans);
+            }
+        };
     }
+    impl_array_scalr_test!(
+        add_scalar_f32,
+        vec![1.0, 2.0, 3.0, 4.0],
+        2.,
+        vec![3.0, 4.0, 5.0, 6.0],
+        f32,
+        array_scalar_add
+    );
+
+    impl_array_scalr_test!(
+        add_scalar_f64,
+        vec![1.0, 2.0, 3.0, 4.0],
+        2.,
+        vec![3.0, 4.0, 5.0, 6.0],
+        f64,
+        array_scalar_add
+    );
+
+    impl_array_scalr_test!(
+        sub_scalar_f32,
+        vec![1.0, 2.0, 3.0, 4.0],
+        2.,
+        vec![-1.0, 0.0, 1.0, 2.0],
+        f32,
+        array_scalar_sub
+    );
+
+    impl_array_scalr_test!(
+        sub_scalar_f64,
+        vec![1.0, 2.0, 3.0, 4.0],
+        2.,
+        vec![-1.0, 0.0, 1.0, 2.0],
+        f64,
+        array_scalar_sub
+    );
+
+    impl_array_scalr_test!(
+        mul_scalar_f32,
+        vec![1.0, 2.0, 3.0, 4.0],
+        2.,
+        vec![2.0, 4.0, 6.0, 8.0],
+        f32,
+        array_scalar_mul
+    );
+
+    impl_array_scalr_test!(
+        mul_scalar_f64,
+        vec![1.0, 2.0, 3.0, 4.0],
+        2.,
+        vec![2.0, 4.0, 6.0, 8.0],
+        f64,
+        array_scalar_mul
+    );
+
+    impl_array_scalr_test!(
+        div_scalar_f32,
+        vec![1.0, 2.0, 3.0, 4.0],
+        2.,
+        vec![0.5, 1.0, 1.5, 2.0],
+        f32,
+        array_scalar_div
+    );
+
+    impl_array_scalr_test!(
+        div_scalar_f64,
+        vec![1.0, 2.0, 3.0, 4.0],
+        2.,
+        vec![0.5, 1.0, 1.5, 2.0],
+        f64,
+        array_scalar_div
+    );
 }
