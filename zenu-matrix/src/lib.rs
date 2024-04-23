667f4f7f30689ff4cb5ccffcf9035eca8ee9c234
bokutotu <mushin.hudoushin@gmail.com>
2024-04-24 01:02:12 +0900

    refactor: Update matrix operations in zenu-matrix
    This commit refactors the zenu-matrix module. It introduces changes in
    the way matrices are handled, specifically in the constructor,
    element-wise operations, and matrix formatting. The changes aim to
    improve the efficiency and readability of the code. The commit also
    removes some redundant code and files, and updates the unit tests
    accordingly.

--- zenu-matrix/src/lib.rs
+++ zenu-matrix/src/lib.rs
@@ -1,14 +1,12 @@
 // pub mod blas;
 // pub mod concat;
-// pub mod constructor;
+pub mod constructor;
 // pub mod cpu_blas;
 // pub mod cpu_element_wise;
 pub mod dim;
-// pub mod element_wise;
 pub mod index;
 pub mod matrix;
 // pub mod matrix_blas;
-// pub mod matrix_impl;
 // pub mod matrix_iter;
 // pub mod memory;
 // pub mod memory_impl;
@@ -22,4 +20,4 @@ pub mod slice;
 mod gpu_method;
 
 // mod impl_ops;
-// mod matrix_format;
+mod matrix_format;
