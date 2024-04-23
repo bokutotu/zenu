8385a2b77bae3e3e24f4dd0db8171371934d2e8f
bokutotu <mushin.hudoushin@gmail.com>
2024-04-24 01:46:15 +0900

    feat(zenu-matrix): remove unused memory modules
    The memory and memory_impl modules in the zenu-matrix library were
    removed as they were not being used. This change simplifies the codebase
    and reduces the maintenance burden.

--- zenu-matrix/src/lib.rs
+++ zenu-matrix/src/lib.rs
@@ -2,14 +2,11 @@
 // pub mod concat;
 pub mod constructor;
 // pub mod cpu_blas;
-// pub mod cpu_element_wise;
 pub mod dim;
 pub mod index;
 pub mod matrix;
 // pub mod matrix_blas;
 // pub mod matrix_iter;
-// pub mod memory;
-// pub mod memory_impl;
 pub mod num;
 // pub mod operation;
 pub mod device;
