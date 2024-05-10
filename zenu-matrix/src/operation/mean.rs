7522e5aacab8ef4c8abf450cc3ce1d2a9c8f0414
bokutotu <mushin.hudoushin@gmail.com>
2024-05-10 19:12:30 +0900

    add mean

--- zenu-matrix/src/operation/mean.rs
+++ zenu-matrix/src/operation/mean.rs
@@ -1,32 +1,24 @@
 use crate::{
+    device::Device,
     dim::{DimDyn, DimTrait},
-    matrix::{MatrixBase, OwnedMatrix, ToViewMatrix},
-    matrix_impl::{Matrix, OwnedMatrixDyn},
-    memory::{Memory, ToViewMemory},
-    memory_impl::OwnedMem,
+    matrix::{Matrix, Owned, Repr},
     num::Num,
 };
 
-use super::{asum::Asum, sum::MatrixSum};
-
-pub trait Mean<T: Num> {
-    fn mean(&self, axis: Option<usize>, keep_dim: bool) -> Matrix<OwnedMem<T>, DimDyn>;
-}
-
-impl<T: Num, M: Memory<Item = T> + ToViewMemory, D: DimTrait> Mean<T> for Matrix<M, D> {
-    fn mean(&self, axis: Option<usize>, keep_dim: bool) -> Matrix<OwnedMem<T>, DimDyn> {
+impl<T: Num, R: Repr<Item = T>, S: DimTrait, D: Device> Matrix<R, S, D> {
+    pub fn mean(&self, axis: Option<usize>, keep_dim: bool) -> Matrix<Owned<T>, DimDyn, D> {
         match axis {
             Some(axis) => {
                 let sum_axis_num_elm = self.shape()[axis];
 
-                let sum = self.to_view().into_dyn_dim().sum(axis, keep_dim);
+                let sum = self.to_ref().into_dyn_dim().sum(axis, keep_dim);
                 sum / T::from_usize(sum_axis_num_elm)
             }
             None => {
-                let asum = self.to_view().asum();
+                let asum = self.to_ref().asum();
                 let num_elm = self.shape().num_elm();
                 let mean = asum / T::from_usize(num_elm);
-                OwnedMatrixDyn::from_vec(vec![mean], [])
+                Matrix::from_vec(vec![mean], [])
             }
         }
     }
