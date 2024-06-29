a48638f07c618e2e2937fc24a147801141b32163
bokutotu <mushin.hudoushin@gmail.com>
2024-06-29 22:36:30 +0900

    add test for serde

--- zenu-matrix/src/impl_serde.rs
+++ zenu-matrix/src/impl_serde.rs
@@ -1,13 +1,10 @@
 use serde::{Deserialize, Serialize};
-use std::marker::PhantomData;
-use std::str::FromStr;
 
 use crate::device::cpu::Cpu;
 use crate::device::DeviceBase;
 use crate::dim::{DimDyn, DimTrait};
-use crate::matrix::{Matrix, Owned};
+use crate::matrix::{Matrix, Owned, Ptr};
 use crate::num::Num;
-use crate::shape_stride::ShapeStride;
 
 // 中間構造体
 #[derive(Serialize, Deserialize)]
@@ -16,6 +13,7 @@ struct MatrixSerializeData<T> {
     stride: Vec<usize>,
     data: Vec<T>,
     data_type: String,
+    ptr_offset: usize,
 }
 
 impl<T, D> Matrix<Owned<T>, DimDyn, D>
@@ -23,7 +21,6 @@ where
     T: Num,
     D: DeviceBase,
 {
-    // MatrixSerializeDataへの変換メソッド
     fn to_serialize_data(&self) -> MatrixSerializeData<T> {
         let shape = self.shape().slice().to_vec();
         let stride = self.stride().slice().to_vec();
@@ -34,15 +31,22 @@ where
             .to_vec();
         let data_type = std::any::type_name::<T>().to_string();
 
+        let ptr_offset = self.offset();
+
         MatrixSerializeData {
             shape,
             stride,
             data,
             data_type,
+            ptr_offset,
         }
     }
+}
 
-    // MatrixSerializeDataからMatrixを作成するメソッド
+impl<T> Matrix<Owned<T>, DimDyn, Cpu>
+where
+    T: Num,
+{
     fn from_serialize_data(data: MatrixSerializeData<T>) -> Result<Self, String> {
         if std::any::type_name::<T>() != data.data_type {
             return Err("Data type mismatch".to_string());
@@ -50,13 +54,13 @@ where
 
         let shape = DimDyn::from(&data.shape as &[usize]);
         let stride = DimDyn::from(&data.stride as &[usize]);
-        // let matrix = Self::from_vec(data.data, shape);
-        //
-        // Ok(matrix.with_stride(stride))
-        let mut matrix = Matrix::from_vec(data.data, shape);
-        let shape_stride = ShapeStride::new(shape, stride);
-        matrix.update_shape_stride(shape_stride);
-        Ok(matrix)
+
+        let mut data_cloned = data.data.clone();
+        let ptr =
+            Ptr::<Owned<T>, Cpu>::new(data_cloned.as_mut_ptr(), data.data.len(), data.ptr_offset);
+        std::mem::forget(data_cloned);
+
+        Ok(Matrix::new(ptr, shape, stride))
     }
 }
 
@@ -82,8 +86,12 @@ where
     where
         De: serde::Deserializer<'de>,
     {
-        let data = MatrixSerializeData::deserialize(deserializer)?;
-        Self::from_serialize_data(data).map_err(serde::de::Error::custom)
+        let data = MatrixSerializeData::<T>::deserialize(deserializer)?;
+        let cpu_matrix =
+            Matrix::<Owned<T>, DimDyn, Cpu>::from_serialize_data(data).map_err(|e| {
+                serde::de::Error::custom(format!("Failed to deserialize matrix: {}", e))
+            })?;
+        Ok(cpu_matrix.to::<D>())
     }
 }
 
@@ -91,23 +99,20 @@ where
 mod tests {
     use super::*;
     use serde_json;
+    use zenu_test::assert_mat_eq_epsilon;
 
     #[test]
     fn test_matrix_serialization_deserialization() {
-        // 1. テスト用のMatrixを作成
         let data = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
         let shape: Vec<usize> = vec![2, 3];
         let matrix: Matrix<Owned<f64>, DimDyn, Cpu> =
             Matrix::from_vec(data, DimDyn::from(&shape as &[usize]));
 
-        // 2. Matrixをシリアライズ
         let serialized = serde_json::to_string(&matrix).expect("Failed to serialize matrix");
 
-        // 3. シリアライズされた文字列をデシリアライズ
         let deserialized: Matrix<Owned<f64>, DimDyn, Cpu> =
             serde_json::from_str(&serialized).expect("Failed to deserialize matrix");
 
-        // 4. 元のMatrixとデシリアライズされたMatrixを比較
         assert_eq!(matrix.shape(), deserialized.shape());
         assert_eq!(matrix.stride(), deserialized.stride());
 
@@ -115,12 +120,7 @@ mod tests {
         let deserialized_data = deserialized.to_vec();
         assert_eq!(original_data, deserialized_data);
 
-        // 5. 値を個別に確認
-        for i in 0..2 {
-            for j in 0..3 {
-                assert_eq!(matrix.index_item([i, j]), deserialized.index_item([i, j]));
-            }
-        }
+        assert_mat_eq_epsilon!(matrix, deserialized, 1e-6);
     }
 
     #[test]
@@ -143,12 +143,13 @@ mod tests {
     1
   ],
   "data": [
-    1,
-    2,
-    3,
-    4
+    1.0,
+    2.0,
+    3.0,
+    4.0
   ],
-  "data_type": "f32"
+  "data_type": "f32",
+  "ptr_offset": 0
 }"#;
 
         assert_eq!(serialized, expected_json);
@@ -156,15 +157,15 @@ mod tests {
 
     #[test]
     fn test_matrix_deserialization_error() {
-        // データ型が一致しない場合のテスト
         let invalid_json = r#"{
             "shape": [2, 2],
             "stride": [2, 1],
             "data": [1.0, 2.0, 3.0, 4.0],
-            "data_type": "f64"
+            "data_type": "f64",
+            "ptr_offset": 0
         }"#;
 
-        let result: Result<Matrix<Owned<f64>, DimDyn, Cpu>, _> = serde_json::from_str(invalid_json);
+        let result: Result<Matrix<Owned<f32>, DimDyn, Cpu>, _> = serde_json::from_str(invalid_json);
         assert!(result.is_err());
     }
 }
