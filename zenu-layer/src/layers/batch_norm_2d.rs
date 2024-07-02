e22109859caf7b1e77f5348ba6564c5fbbb20e27
bokutotu <mushin.hudoushin@gmail.com>
2024-07-03 05:37:33 +0900

    add serde to batch_norm_2d

--- zenu-layer/src/layers/batch_norm_2d.rs
+++ zenu-layer/src/layers/batch_norm_2d.rs
@@ -1,3 +1,4 @@
+use serde::{ser::SerializeStruct, Deserialize, Serialize};
 use zenu_autograd::{
     creator::{ones::ones, zeros::zeros},
     functions::batch_norm::{batch_norm_2d, BatchNorm2dAutoGradConfig},
@@ -14,10 +15,215 @@ use crate::Layer;
 pub struct BatchNorm2d<T: Num, D: Device> {
     config: BatchNorm2dAutoGradConfig<T>,
     momentum: f64,
+    input_shape: DimDyn,
     scale: Variable<T, D>,
     bias: Variable<T, D>,
     mean: Variable<T, D>,
     variance: Variable<T, D>,
+}
+
+impl<T: Num, D: Device> Serialize for BatchNorm2d<T, D> {
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        let mut state = serializer.serialize_struct("BatchNorm2d", 6)?;
+        state.serialize_field("momentum", &self.momentum)?;
+        state.serialize_field("input_shape", &self.input_shape)?;
+        state.serialize_field("scale", &self.scale)?;
+        state.serialize_field("bias", &self.bias)?;
+        state.serialize_field("mean", &self.mean)?;
+        state.serialize_field("variance", &self.variance)?;
+
+        state.end()
+    }
+}
+
+impl<'de, T: Num + Deserialize<'de>, D: Device> Deserialize<'de> for BatchNorm2d<T, D> {
+    fn deserialize<Ds: serde::Deserializer<'de>>(deserializer: Ds) -> Result<Self, Ds::Error> {
+        const FIELDS: &[&str] = &[
+            "momentum",
+            "input_shape",
+            "scale",
+            "bias",
+            "mean",
+            "variance",
+        ];
+
+        enum Field {
+            Momentum,
+            InputShape,
+            Scale,
+            Bias,
+            Mean,
+            Variance,
+        }
+
+        impl<'de> Deserialize<'de> for Field {
+            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                struct FieldVisitor;
+
+                impl<'de> serde::de::Visitor<'de> for FieldVisitor {
+                    type Value = Field;
+
+                    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                        formatter.write_str("field identifier")
+                    }
+
+                    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+                    where
+                        E: serde::de::Error,
+                    {
+                        match value {
+                            "momentum" => Ok(Field::Momentum),
+                            "input_shape" => Ok(Field::InputShape),
+                            "scale" => Ok(Field::Scale),
+                            "bias" => Ok(Field::Bias),
+                            "mean" => Ok(Field::Mean),
+                            "variance" => Ok(Field::Variance),
+                            _ => Err(serde::de::Error::unknown_field(value, FIELDS)),
+                        }
+                    }
+                }
+
+                deserializer.deserialize_identifier(FieldVisitor)
+            }
+        }
+
+        struct BatchNorm2dVisitor<T: Num, D: Device> {
+            marker: std::marker::PhantomData<BatchNorm2d<T, D>>,
+        }
+
+        impl<'de, T: Num + Deserialize<'de>, D: Device> serde::de::Visitor<'de>
+            for BatchNorm2dVisitor<T, D>
+        {
+            type Value = BatchNorm2d<T, D>;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("struct BatchNorm2d")
+            }
+
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+            where
+                A: serde::de::SeqAccess<'de>,
+            {
+                let momentum = seq
+                    .next_element()?
+                    .ok_or_else(|| serde::de::Error::invalid_length(0, &self))?;
+                let input_shape: Vec<usize> = seq
+                    .next_element()?
+                    .ok_or_else(|| serde::de::Error::invalid_length(1, &self))?;
+                let scale = seq
+                    .next_element()?
+                    .ok_or_else(|| serde::de::Error::invalid_length(2, &self))?;
+                let bias = seq
+                    .next_element()?
+                    .ok_or_else(|| serde::de::Error::invalid_length(3, &self))?;
+                let mean = seq
+                    .next_element()?
+                    .ok_or_else(|| serde::de::Error::invalid_length(4, &self))?;
+                let variance = seq
+                    .next_element()?
+                    .ok_or_else(|| serde::de::Error::invalid_length(5, &self))?;
+
+                let config = BatchNorm2dAutoGradConfig::new(&input_shape);
+                let input_shape = DimDyn::from(&input_shape as &[usize]);
+
+                Ok(BatchNorm2d {
+                    config,
+                    momentum,
+                    input_shape,
+                    scale,
+                    bias,
+                    mean,
+                    variance,
+                })
+            }
+
+            fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+            where
+                A: serde::de::MapAccess<'de>,
+            {
+                let mut momentum = None;
+                let mut input_shape = None;
+                let mut scale = None;
+                let mut bias = None;
+                let mut mean = None;
+                let mut variance = None;
+                while let Some(key) = map.next_key()? {
+                    match key {
+                        Field::Momentum => {
+                            if momentum.is_some() {
+                                return Err(serde::de::Error::duplicate_field("momentum"));
+                            }
+                            momentum = Some(map.next_value()?);
+                        }
+                        Field::InputShape => {
+                            if input_shape.is_some() {
+                                return Err(serde::de::Error::duplicate_field("input_shape"));
+                            }
+                            input_shape = Some(map.next_value()?);
+                        }
+                        Field::Scale => {
+                            if scale.is_some() {
+                                return Err(serde::de::Error::duplicate_field("scale"));
+                            }
+                            scale = Some(map.next_value()?);
+                        }
+                        Field::Bias => {
+                            if bias.is_some() {
+                                return Err(serde::de::Error::duplicate_field("bias"));
+                            }
+                            bias = Some(map.next_value()?);
+                        }
+                        Field::Mean => {
+                            if mean.is_some() {
+                                return Err(serde::de::Error::duplicate_field("mean"));
+                            }
+                            mean = Some(map.next_value()?);
+                        }
+                        Field::Variance => {
+                            if variance.is_some() {
+                                return Err(serde::de::Error::duplicate_field("variance"));
+                            }
+                            variance = Some(map.next_value()?);
+                        }
+                    }
+                }
+
+                let momentum =
+                    momentum.ok_or_else(|| serde::de::Error::missing_field("momentum"))?;
+                let input_shape: Vec<usize> =
+                    input_shape.ok_or_else(|| serde::de::Error::missing_field("input_shape"))?;
+                let scale = scale.ok_or_else(|| serde::de::Error::missing_field("scale"))?;
+                let bias = bias.ok_or_else(|| serde::de::Error::missing_field("bias"))?;
+                let mean = mean.ok_or_else(|| serde::de::Error::missing_field("mean"))?;
+                let variance =
+                    variance.ok_or_else(|| serde::de::Error::missing_field("variance"))?;
+
+                let config = BatchNorm2dAutoGradConfig::new(&input_shape);
+                let input_shape = DimDyn::from(&input_shape as &[usize]);
+
+                Ok(BatchNorm2d {
+                    config,
+                    momentum,
+                    input_shape,
+                    scale,
+                    bias,
+                    mean,
+                    variance,
+                })
+            }
+        }
+
+        deserializer.deserialize_struct(
+            "BatchNorm2d",
+            FIELDS,
+            BatchNorm2dVisitor {
+                marker: std::marker::PhantomData,
+            },
+        )
+    }
 }
 
 impl<T: Num, D: Device> Layer<T, D> for BatchNorm2d<T, D> {
@@ -79,6 +285,7 @@ impl<T: Num, D: Device> BatchNorm2d<T, D> {
         Self {
             config,
             momentum,
+            input_shape,
             scale,
             bias,
             mean,
