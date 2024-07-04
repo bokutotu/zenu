79deab5c81a68a59ae0660f40aff01101a60bad2
bokutotu <mushin.hudoushin@gmail.com>
2024-07-04 19:45:43 +0900

    update

--- zenu-matrix/src/nn/conv2d/conv2d_bckwd_filter_cpu.rs
+++ zenu-matrix/src/nn/conv2d/conv2d_bckwd_filter_cpu.rs
@@ -15,7 +15,7 @@ pub(super) fn conv2d_bckwd_fileter<T: Num, D: Device>(
     stride: (usize, usize),
 ) -> Matrix<Owned<T>, DimDyn, D> {
     let kernel_h_w = (kernel_shape[2], kernel_shape[3]);
-    let Im2ColRes { mut col, .. } = im2col(input, kernel_h_w, stride, padding);
+    let Im2ColRes { mut col, .. } = im2col(input, kernel_h_w, stride, padding, true);
 
     let gradient_output = gradient_output.transpose_swap_index_new_matrix(0, 1);
     let gradient_output_shape = gradient_output.shape();
---
+++ zenu-matrix/src/nn/pool2d.rs
@@ -0,0 +1,76 @@
+use crate::{
+    device::{cpu::Cpu, Device},
+    dim::DimDyn,
+    matrix::{Matrix, Ref},
+    num::Num,
+};
+
+#[cfg(feature = "nvidia")]
+use zenu_cuda::cudnn::pooling::Pool2d;
+
+#[cfg(feature = "nvidia")]
+use crate::device::nvidia::Nvidia;
+
+use super::im2col::{im2col, Im2ColRes};
+
+pub struct Pool2dConfig<T: Num> {
+    #[cfg(feature = "nvidia")]
+    pub config: Pool2d<T>,
+    _phantom: std::marker::PhantomData<T>,
+}
+
+pub trait Pool2dImpl: Device {
+    fn pool2d<T: Num>(
+        input: Matrix<Ref<&T>, DimDyn, Self>,
+        output: Matrix<Ref<&mut T>, DimDyn, Self>,
+        kernel_shape: (usize, usize),
+        stride: (usize, usize),
+        padding: (usize, usize),
+        config: &Pool2dConfig<T>,
+    ) -> Result<(), String>;
+
+    fn pool2d_backward<T: Num>(
+        input: Matrix<Ref<&T>, DimDyn, Self>,
+        input_grad: Matrix<Ref<&mut T>, DimDyn, Self>,
+        output: Matrix<Ref<&T>, DimDyn, Self>,
+        output_grad: Matrix<Ref<&T>, DimDyn, Self>,
+        kernel_shape: (usize, usize),
+        stride: (usize, usize),
+        padding: (usize, usize),
+        config: &Pool2dConfig<T>,
+    );
+}
+
+impl Pool2dImpl for Cpu {
+    fn pool2d<T: Num>(
+        input: Matrix<Ref<&T>, DimDyn, Self>,
+        output: Matrix<Ref<&mut T>, DimDyn, Self>,
+        kernel_shape: (usize, usize),
+        stride: (usize, usize),
+        padding: (usize, usize),
+        config: &Pool2dConfig<T>,
+    ) -> Result<(), String> {
+        let Im2ColRes { mut col, .. } = im2col(input, kernel_shape, stride, padding, false);
+        let col_shape = col.shape();
+        let col = col.reshape_no_alloc_owned([
+            col_shape[0],
+            col_shape[1],
+            col_shape[2] * col_shape[3],
+            col_shape[4],
+            col_shape[5],
+        ]);
+    }
+
+    fn pool2d_backward<T: Num>(
+        input: Matrix<Ref<&T>, DimDyn, Self>,
+        input_grad: Matrix<Ref<&mut T>, DimDyn, Self>,
+        output: Matrix<Ref<&T>, DimDyn, Self>,
+        output_grad: Matrix<Ref<&T>, DimDyn, Self>,
+        kernel_shape: (usize, usize),
+        stride: (usize, usize),
+        padding: (usize, usize),
+        config: &Pool2dConfig<T>,
+    ) {
+        todo!();
+    }
+}
