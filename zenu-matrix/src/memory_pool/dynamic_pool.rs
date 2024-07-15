d091be4b628966367e934144b69a7366aa42ccd1
bokutotu <mushin.hudoushin@gmail.com>
2024-07-16 01:43:27 +0900

    update

--- zenu-matrix/src/memory_pool/dynamic_pool.rs
+++ zenu-matrix/src/memory_pool/dynamic_pool.rs
@@ -16,10 +16,42 @@ pub struct DynMemPool<D: DeviceBase> {
 }
 
 impl<D: DeviceBase> DynMemPool<D> {
-    fn try_alloc(&mut self, bytes: usize) -> Result<*mut u8, ()> {
-        match self.unused_buffers.range_mut((Included(bytes), Unbounded)) {
-            Some(buffer) => {}
-            None => {}
+    pub fn try_alloc(&mut self, bytes: usize) -> Result<*mut u8, ()> {
+        // match self
+        //     .unused_buffers
+        //     .range_mut((Included(bytes), Unbounded))
+        //     .next()
+        // {
+        match self.unused_buffers.get_mut(&bytes) {
+            Some(buffers) => {
+                let buffer = buffers.pop().unwrap();
+                let ptr = buffer.borrow().start_ptr();
+                self.used_buffers.insert(ptr, buffer);
+                Ok(ptr)
+            }
+            None => {
+                let buffer = Rc::new(RefCell::new(DynBuffer::new(bytes)?));
+                let ptr = buffer.borrow().start_ptr();
+                self.used_buffers.insert(ptr, buffer);
+                Ok(ptr)
+            }
         }
+    }
+
+    pub fn try_free(&mut self, ptr: *mut u8) -> Result<(), ()> {
+        let buffer = self.used_buffers.remove(&ptr).ok_or(())?;
+        let bytes = buffer.borrow().bytes();
+        self.unused_buffers
+            .entry(bytes)
+            .or_insert_with(Vec::new)
+            .push(buffer);
+        Ok(())
+    }
+
+    fn smallest_unused_bytes_over_request(&self, bytes: usize) -> Option<usize> {
+        self.unused_buffers
+            .range((Included(&bytes), Unbounded))
+            .next()
+            .map(|(unused_bytes, _)| *unused_bytes)
     }
 }
