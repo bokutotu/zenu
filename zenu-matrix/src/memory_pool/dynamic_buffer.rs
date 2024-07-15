85f94b01014cbea89288e848953beaa80c579750
bokutotu <mushin.hudoushin@gmail.com>
2024-07-16 01:43:27 +0900

    update

--- zenu-matrix/src/memory_pool/dynamic_buffer.rs
+++ zenu-matrix/src/memory_pool/dynamic_buffer.rs
@@ -4,22 +4,20 @@ use super::data_ptr::DataPtr;
 
 pub struct DynBuffer<D: DeviceBase> {
     data_ptr: DataPtr<D>,
-    is_used: bool,
 }
 
 impl<D: DeviceBase> DynBuffer<D> {
     pub fn new(bytes: usize) -> Result<Self, ()> {
         Ok(DynBuffer {
             data_ptr: DataPtr::new(bytes)?,
-            is_used: false,
         })
     }
 
-    pub fn is_used(&self) -> bool {
-        self.is_used
+    pub fn start_ptr(&self) -> *mut u8 {
+        self.data_ptr.ptr
     }
 
-    pub fn set_used(&mut self, is_used: bool) {
-        self.is_used = is_used;
+    pub fn bytes(&self) -> usize {
+        self.data_ptr.bytes
     }
 }
