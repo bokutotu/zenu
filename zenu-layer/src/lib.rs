8d827d9023d5927257ead6f6b15d32131c12a80f
bokutotu <mushin.hudoushin@gmail.com>
2024-07-23 01:43:26 +0900

    fix name

--- zenu-layer/src/lib.rs
+++ zenu-layer/src/lib.rs
@@ -8,7 +8,7 @@ pub trait Module<T: Num, D: Device> {
     fn call(&self, input: Variable<T, D>) -> Variable<T, D>;
 }
 
-pub trait Parameterizes<T: Num, D: Device> {
+pub trait Parameteres<T: Num, D: Device> {
     fn weights(&self) -> Vec<&Variable<T, D>>;
     fn biases(&self) -> Vec<&Variable<T, D>>;
     fn parameters(&self) -> Vec<&Variable<T, D>> {
