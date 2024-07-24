62a8b77e3386a9bf88a13e00747ac9b809513044
bokutotu <mushin.hudoushin@gmail.com>
2024-07-24 20:08:59 +0900

    update state_dict

--- zenu-macros/src/lib.rs
+++ zenu-macros/src/lib.rs
@@ -1,39 +1,97 @@
 use proc_macro::TokenStream;
-use proc_macro2::TokenStream as TokenStream2;
+use proc_macro2::{Span, TokenStream as TokenStream2};
 use quote::quote;
-use syn::{parse_macro_input, parse_quote, Data, DeriveInput, Field};
+use syn::{parse_macro_input, Attribute, Data, DeriveInput, Field, Ident, Lit, Meta, NestedMeta};
 
 #[proc_macro_derive(ZenuModel, attributes(zenu))]
-pub fn my_derive(input: TokenStream) -> TokenStream {
+pub fn zenu_derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let name = &input.ident;
-    let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
 
-    let state_dict_impl = impl_state_dict(name, &impl_generics, &ty_generics, where_clause);
+    let (nums, devices) = input
+        .clone()
+        .attrs
+        .into_iter()
+        .filter_map(extract_bounds)
+        .next()
+        .unwrap_or_default();
+
+    let state_dict = impl_state_dict(&input, &nums, &devices);
     let parameters_impl = impl_parameters(&input);
 
     let expanded = quote! {
-        // #[automatically_derived]
-        // #[derive(::serde::Serialize, ::serde::Deserialize)]
-        // #[serde(bound(deserialize = "T: ::zenu_matrix::num::Num + ::serde::Deserialize<'de>"))]
-        // #modified_struct
-        //
-        // #state_dict_impl
-
+        #state_dict
         #parameters_impl
     };
 
     TokenStream::from(expanded)
 }
 
-fn impl_state_dict(
-    name: &syn::Ident,
-    impl_generics: &syn::ImplGenerics,
-    ty_generics: &syn::TypeGenerics,
-    where_clause: Option<&syn::WhereClause>,
-) -> TokenStream2 {
+/// Extracts numeric and device bounds from a zenu attribute
+fn extract_bounds(attr: Attribute) -> Option<(Vec<String>, Vec<String>)> {
+    match attr.parse_meta() {
+        Ok(Meta::List(meta_list)) if meta_list.path.is_ident("zenu") => {
+            let mut num_bounds = Vec::new();
+            let mut device_bounds = Vec::new();
+
+            for nested in meta_list.nested {
+                if let NestedMeta::Meta(Meta::List(inner_list)) = nested {
+                    if inner_list.path.is_ident("bound") {
+                        for inner_nested in inner_list.nested {
+                            if let NestedMeta::Meta(Meta::NameValue(name_value)) = inner_nested {
+                                if let Lit::Str(lit_str) = &name_value.lit {
+                                    let values: Vec<String> = lit_str
+                                        .value()
+                                        .split(',')
+                                        .map(str::trim)
+                                        .map(String::from)
+                                        .collect();
+
+                                    if name_value.path.is_ident("num") {
+                                        num_bounds = values;
+                                    } else if name_value.path.is_ident("device") {
+                                        device_bounds = values;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Some((num_bounds, device_bounds))
+        }
+        _ => None,
+    }
+}
+
+fn impl_state_dict(input: &DeriveInput, nums: &[String], devices: &[String]) -> TokenStream2 {
+    let name = &input.ident;
+    let (_, ty_generics, where_clause) = input.generics.split_for_impl();
+
+    let nums_trait_bound = nums.iter().map(|num| {
+        let num = Ident::new(num, Span::call_site());
+        quote! {
+            #num: ::zenu_matrix::num::Num + ::serde::Serialize + ::serde::Deserialize<'de>
+        }
+    });
+
+    let devices_trait_bound = devices.iter().map(|device| {
+        let device = Ident::new(device, Span::call_site());
+        quote! {
+            #device: ::zenu_matrix::device::Device + ::serde::Serialize + ::serde::Deserialize<'de>
+        }
+    });
+
+    let bounds: Vec<_> = nums_trait_bound.chain(devices_trait_bound).collect();
+
+    let bounds_quote = if !bounds.is_empty() {
+        quote! { #(#bounds),* }
+    } else {
+        quote! {}
+    };
+
     quote! {
-        impl #impl_generics ::zenu_layer::StateDict<'de> for #name #ty_generics #where_clause {  }
+        impl <'de, #bounds_quote> ::zenu_layer::StateDict<'de> for #name #ty_generics #where_clause {}
     }
 }
 
@@ -46,7 +104,6 @@ fn impl_parameters(input: &DeriveInput) -> TokenStream2 {
         _ => panic!("ZenuModel only supports structs"),
     };
 
-    // get not #[zenu(skip)] fields
     let fields = fields.iter().filter(|field| !has_zenu_skip_attr(field));
 
     let weights_code = fields.clone().map(|field| {
