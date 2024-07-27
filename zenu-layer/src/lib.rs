71bdbcd3715589b93f5f9364e0b0c3efe2ce4755
bokutotu <mushin.hudoushin@gmail.com>
2024-07-27 15:06:03 +0900

    update

--- zenu-layer/src/lib.rs
+++ zenu-layer/src/lib.rs
@@ -17,23 +17,3 @@ pub trait Parameters<T: Num, D: Device> {
         params
     }
 }
-
-pub trait StateDict<'de>: Serialize + Deserialize<'de> {
-    fn to_json(&self) -> String {
-        serde_json::to_string(self).unwrap()
-    }
-
-    #[allow(clippy::must_use_candidate)]
-    fn from_json(json: &'de str) -> Self {
-        serde_json::from_str(json).unwrap()
-    }
-
-    fn to_bytes(&self) -> Vec<u8> {
-        bincode::serialize(self).unwrap()
-    }
-
-    #[allow(clippy::must_use_candidate)]
-    fn from_bytes(bytes: &'de [u8]) -> Self {
-        bincode::deserialize(bytes).unwrap()
-    }
-}
