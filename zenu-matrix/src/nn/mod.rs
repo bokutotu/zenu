f50bab0cbdc1f1c68b2f28b52a9f025968615999
bokutotu <mushin.hudoushin@gmail.com>
2024-08-08 00:48:23 +0900

    update gpu tests

--- zenu-matrix/src/nn/mod.rs
+++ zenu-matrix/src/nn/mod.rs
@@ -1,5 +1,36 @@
+use crate::device::DeviceBase;
+
 pub mod batch_norm;
 pub mod col2im;
 pub mod conv2d;
+pub mod dropout;
 pub mod im2col;
 pub mod pool2d;
+
+#[allow(unused)]
+pub(crate) struct NNCache<D: DeviceBase> {
+    pub(crate) bytes: usize,
+    pub(crate) ptr: *mut u8,
+    _device: std::marker::PhantomData<D>,
+}
+
+impl<D: DeviceBase> NNCache<D> {
+    #[allow(unused)]
+    pub(crate) fn new(bytes: usize) -> Self {
+        let ptr = D::alloc(bytes).unwrap();
+        if ptr.is_null() {
+            panic!("Failed to allocate memory");
+        }
+        Self {
+            bytes,
+            ptr: ptr as *mut u8,
+            _device: std::marker::PhantomData,
+        }
+    }
+}
+
+impl<D: DeviceBase> Drop for NNCache<D> {
+    fn drop(&mut self) {
+        D::drop_ptr(self.ptr);
+    }
+}
