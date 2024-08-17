36058e0bd868ff0e56801baa09219525ad44726e
bokutotu <mushin.hudoushin@gmail.com>
2024-08-17 12:37:25 +0000

    remove useless files

--- zenu-matrix/src/nn/rnn/mod.rs
+++ zenu-matrix/src/nn/rnn/mod.rs
@@ -1,12 +1,7 @@
 mod config;
-// mod cpu;
 mod functions;
-// #[cfg(feature = "nvidia")]
-// mod nvidia;
 mod params;
-// mod traits;
 
 pub use config::*;
-pub use params::*;
-// pub use traits::*;
 pub use functions::*;
+pub use params::*;
