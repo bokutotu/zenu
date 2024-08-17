d95222e7e3bb38ead79b6c300c9cd3d64101be55
bokutotu <mushin.hudoushin@gmail.com>
2024-08-17 11:13:11 +0000

    Add impl rnn bkwd weight

--- zenu-matrix/src/nn/rnn/functions.rs
+++ zenu-matrix/src/nn/rnn/functions.rs
@@ -69,6 +69,57 @@ pub fn rnn_fwd<T: Num>(
     RNNOutput { y, hy }
 }
 
+fn rnn_bkwd_data_shape_check(
+    x: DimDyn,
+    y: DimDyn,
+    dy: DimDyn,
+    hx: Option<DimDyn>,
+    dhy: Option<DimDyn>,
+    config: &RNNConfig,
+) {
+    if x.len() != 3 {
+        panic!("Input shape must be 3D");
+    }
+    if x[1] != config.get_batch_size() {
+        panic!("Batch size mismatch");
+    }
+    if x[2] != config.get_input_size() {
+        panic!("Input size mismatch");
+    }
+    if y.len() != 3 {
+        panic!("Output shape must be 3D");
+    }
+    if y[1] != config.get_batch_size() {
+        panic!("Batch size mismatch");
+    }
+    if y[2] != config.get_hidden_size() {
+        panic!("Hidden size mismatch");
+    }
+    if y.slice() != dy.slice() {
+        panic!("Output and dy shape mismatch");
+    }
+
+    if hx.is_some() != dhy.is_some() {
+        panic!("hx and dhy must be both None or both Some");
+    }
+
+    if hx.is_none() && dhy.is_none() {
+        return;
+    }
+
+    if hx.slice() != dhy.slice() {
+        panic!("hx and dhy shape mismatch");
+    }
+
+    let d = config.get_num_layers() * if config.is_bidirectional() { 2 } else { 1 };
+    if hx[0] != d {
+        panic!("Number of layers mismatch");
+    }
+    if hx[1] != config.get_hidden_size() {
+        panic!("Hidden size mismatch");
+    }
+}
+
 pub fn rnn_bkwd_data<T: Num>(
     x: Matrix<Ref<&T>, DimDyn, Nvidia>,
     y: Matrix<Ref<&T>, DimDyn, Nvidia>,
@@ -78,7 +129,20 @@ pub fn rnn_bkwd_data<T: Num>(
     config: RNNConfig<T>,
     params: RNNParameters,
 ) -> RNNBkwdDataOutput<T> {
-    rnn_fwd_shape_check(x.shape(), hx.map(|hx| hx.shape()), &config);
+    rnn_bkwd_data_shape_check(
+        x.shape(),
+        y.shape(),
+        dy.shape(),
+        hx.map(|hx| hx.shape()).unwrap_or_else(|| {
+            let d = config.get_num_layers() * if config.is_bidirectional() { 2 } else { 1 };
+            [d, config.get_hidden_size()]
+        }),
+        dhy.map(|dhy| dhy.shape()).unwrap_or_else(|| {
+            let d = config.get_num_layers() * if config.is_bidirectional() { 2 } else { 1 };
+            [d, config.get_hidden_size()]
+        }),
+        &config,
+    );
     let rnn_exe = config.create_executor(true, x.shape()[1]);
     let reserve_size = rnn_exe.get_reserve_size();
     let workspace_size = rnn_exe.get_workspace_size();
@@ -112,13 +176,47 @@ pub fn rnn_bkwd_data<T: Num>(
     RNNBkwdDataOutput { dx, dhx }
 }
 
+fn rnn_bkwd_weights_shape_check(x: DimDyn, hx: Option<DimDyn>, y: DimDyn, config: &RNNConfig) {
+    if x.len() != 3 {
+        panic!("Input shape must be 3D");
+    }
+    if x[1] != config.get_batch_size() {
+        panic!("Batch size mismatch");
+    }
+    if x[2] != config.get_input_size() {
+        panic!("Input size mismatch");
+    }
+    if y.len() != 3 {
+        panic!("Output shape must be 3D");
+    }
+    if y[1] != config.get_batch_size() {
+        panic!("Batch size mismatch");
+    }
+    if y[2] != config.get_hidden_size() {
+        panic!("Hidden size mismatch");
+    }
+
+    let d = config.get_num_layers() * if config.is_bidirectional() { 2 } else { 1 };
+    match hx {
+        Some(hx) => {
+            if hx[0] != d {
+                panic!("Number of layers mismatch");
+            }
+            if hx[1] != config.get_hidden_size() {
+                panic!("Hidden size mismatch");
+            }
+        }
+        None => {}
+    }
+}
+
 pub fn rnn_bkwd_weights<T: Num>(
     x: Matrix<Ref<&T>, DimDyn, Nvidia>,
     hx: Option<Matrix<Ref<&T>, DimDyn, Nvidia>>,
     y: Matrix<Ref<&T>, DimDyn, Nvidia>,
     config: RNNConfig<T>,
 ) -> RNNParameters<T> {
-    rnn_fwd_shape_check(x.shape(), hx.map(|hx| hx.shape()), &config);
+    rnn_bkwd_weights_shape_check(x.shape(), hx.map(|hx| hx.shape()), y.shape(), &config);
     let rnn_exe = config.create_executor(true, x.shape()[1]);
     let reserve_size = rnn_exe.get_reserve_size();
     let workspace_size = rnn_exe.get_workspace_size();
