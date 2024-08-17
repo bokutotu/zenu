11bb3200ed58d0ca30d4cf48c3b68bd14d3388c9
bokutotu <mushin.hudoushin@gmail.com>
2024-08-17 07:08:17 +0000

    kenrel build script update

--- zenu-cuda-kernel-sys/build.rs
+++ zenu-cuda-kernel-sys/build.rs
@@ -21,18 +21,18 @@ fn main() {
         .cuda(true)
         .flag("-cudart=shared")
         .flag("--expt-relaxed-constexpr")
-        .flag("-gencode")
-        .flag("arch=compute_52,code=sm_52") // for (GTX 970, 980, 980 Ti, Titan X).
-        .flag("-gencode")
-        .flag("arch=compute_53,code=sm_53") // for (Jetson TX1).
-        .flag("-gencode")
-        .flag("arch=compute_61,code=sm_61") // for (GTX 1070,1080,1080Ti, Titan Xp).
-        .flag("-gencode")
-        .flag("arch=compute_60,code=sm_60") // for (Tesla P100).
-        .flag("-gencode")
-        .flag("arch=compute_62,code=sm_62") // for (Jetson TX2).
-        .flag("-gencode")
-        .flag("arch=compute_75,code=sm_75") // for (RTX 2080Ti).
+        // .flag("-gencode")
+        // .flag("arch=compute_52,code=sm_52") // for (GTX 970, 980, 980 Ti, Titan X).
+        // .flag("-gencode")
+        // .flag("arch=compute_53,code=sm_53") // for (Jetson TX1).
+        // .flag("-gencode")
+        // .flag("arch=compute_61,code=sm_61") // for (GTX 1070,1080,1080Ti, Titan Xp).
+        // .flag("-gencode")
+        // .flag("arch=compute_60,code=sm_60") // for (Tesla P100).
+        // .flag("-gencode")
+        // .flag("arch=compute_62,code=sm_62") // for (Jetson TX2).
+        // .flag("-gencode")
+        // .flag("arch=compute_75,code=sm_75") // for (RTX 2080Ti).
         .flag("-O3")
         .files(cuda_files)
         .include("kernel/")
