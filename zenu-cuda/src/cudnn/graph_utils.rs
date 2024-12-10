6337ceaf220d6ec24b13a4f6ffe765f89108c3b6
bokutotu <mushin.hudoushin@gmail.com>
2024-12-11 01:50:31 +0900

    update

---
+++ zenu-cuda/src/cudnn/graph_utils.rs
@@ -0,0 +1,35 @@
+use zenu_cudnn_frontend_wrapper_sys::{
+    CudnnFrontendDataType_t, CudnnFrontendError_t, CudnnTensorShapeStride,
+};
+
+#[expect(clippy::cast_possible_wrap)]
+pub fn shape_stride_to_cudnn(shape: &[usize], stride: &[usize]) -> CudnnTensorShapeStride {
+    let num_dims = shape.len();
+    let mut dims = [0_i64; 8];
+    let mut strides = [0_i64; 8];
+    for i in 0..num_dims {
+        dims[i] = shape[i] as i64;
+        strides[i] = stride[i] as i64;
+    }
+    CudnnTensorShapeStride {
+        num_dims,
+        dims,
+        strides,
+    }
+}
+
+pub fn get_cudnn_frontend_type<T>() -> CudnnFrontendDataType_t {
+    if std::any::type_name::<T>() == "f32" {
+        CudnnFrontendDataType_t::DATA_TYPE_FLOAT
+    } else if std::any::type_name::<T>() == "f64" {
+        CudnnFrontendDataType_t::DATA_TYPE_DOUBLE
+    } else {
+        panic!("Unsupported data type");
+    }
+}
+
+pub fn success_or_panic(status: CudnnFrontendError_t) {
+    if status != CudnnFrontendError_t::SUCCESS {
+        panic!("Cudnn frontend error: {:?}", status);
+    }
+}
--- zenu-cudnn-frontend-wrapper-sys/build.rs
+++ zenu-cudnn-frontend-wrapper-sys/build.rs
@@ -5,25 +5,16 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // 環境変数OUT_DIRを取得 (cmake::Configでbuild()すると自動的にこのフォルダにビルド成果物が出力される)
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    // CMakeによるビルド
     let dst = cmake::Config::new("cudnn_frontend_wrapper")
         .define("CMAKE_INSTALL_PREFIX", &out_dir)
         .build();
 
-    // リンク検索パスを追加（libcudnn_frontend_wrapper.aが存在する場所）
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
 
-    // 静的ライブラリのリンク指定
     println!("cargo:rustc-link-lib=static=cudnn_frontend_wrapper");
 
-    // 必要に応じてCUDA、cuDNNのリンクを指定（ライブラリが静的にすべてリンク済みなら不要）
-    // 例: println!("cargo:rustc-link-lib=cudart");
-    //     println!("cargo:rustc-link-lib=cudnn");
-
-    // BindgenでヘッダファイルからRust用バインディングを生成
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let header_path = PathBuf::from(&manifest_dir)
         .join("cudnn_frontend_wrapper")
@@ -39,10 +30,10 @@ fn main() {
                 .display()
         ))
         .clang_arg("-I/usr/local/cuda/include")
+        .rustified_enum(".*")
         .generate()
         .expect("Unable to generate bindings");
 
-    // srcディレクトリにbindings.rsとして出力
     let out_path = PathBuf::from("src").join("bindings.rs");
     bindings
         .write_to_file(&out_path)
