7cc4b41e0d89f71607ac369d9967a02d6fa89589
bokutotu <mushin.hudoushin@gmail.com>
2024-12-30 21:40:53 +0900

    conv biasにテストを書いた

--- zenu-autograd/src/nn/conv/conv_with_bias.rs
+++ zenu-autograd/src/nn/conv/conv_with_bias.rs
@@ -72,7 +72,8 @@ fn conv_bias<T: Num, D: Device>(input: Variable<T, D>, bias: Variable<T, D>) -> 
 }
 
 fn conv_bias_grad<T: Num, D: Device>(d_output: Variable<T, D>) -> Variable<T, D> {
-    let d_bias = alloc(d_output.get_shape());
+    let d_bias_shape = [1, d_output.get_shape()[1], 1, 1];
+    let d_bias = alloc(d_bias_shape);
     let backward = ConvBiasGrad {
         d_output,
         d_bias: d_bias.clone().downgrade(),
@@ -83,15 +84,70 @@ fn conv_bias_grad<T: Num, D: Device>(d_output: Variable<T, D>) -> Variable<T, D>
 }
 
 #[allow(clippy::needless_pass_by_value)]
+#[must_use]
 pub fn conv<T: Num, D: Device>(
     input: Variable<T, D>,
-    kernel: Variable<T, D>,
+    filter: Variable<T, D>,
     bias: Option<Variable<T, D>>,
     config: ConvConfigs<T>,
 ) -> Variable<T, D> {
-    let output = conv_without_bias(input.clone(), kernel, config.clone());
+    let output = conv_without_bias(input.clone(), filter, config.clone());
     match bias {
         Some(bias) => conv_bias(output, bias),
         None => output,
     }
+}
+
+#[cfg(test)]
+mod conv_bias {
+    use std::collections::HashMap;
+
+    use zenu_matrix::{
+        device::{cpu::Cpu, Device},
+        dim::DimDyn,
+        matrix::{Matrix, Owned},
+    };
+    use zenu_test::{assert_val_eq, assert_val_eq_grad, read_test_case_from_json_val, run_test};
+
+    use super::conv;
+    use crate::{nn::conv::ConvConfigs, Variable};
+
+    fn conv_bias<D: Device>() {
+        let map: HashMap<String, Matrix<Owned<f32>, DimDyn, Cpu>> =
+            read_test_case_from_json_val!("../test_data_json/conv_bias.json");
+        let map: HashMap<String, Matrix<Owned<f32>, DimDyn, D>> = map
+            .into_iter()
+            .map(|(key, value)| (key, value.to()))
+            .collect();
+        let x = map.get("input").unwrap().clone();
+        let w = map.get("filter").unwrap().clone();
+        let b = map.get("bias").unwrap().clone();
+        let y = map.get("output").unwrap().clone();
+        let db = map.get("grad_bias").unwrap().clone();
+        let dx = map.get("grad_input").unwrap().clone();
+        let dw = map.get("grad_weight").unwrap().clone();
+
+        let b = b.reshape_no_alloc_owned([1, y.shape()[1], 1, 1]);
+        let db = db.reshape_no_alloc_owned([1, y.shape()[1], 1, 1]);
+
+        let config = ConvConfigs::new(
+            x.shape_stride(),
+            w.shape_stride(),
+            &[1, 1],
+            &[1, 1],
+            &[1, 1],
+        );
+
+        let x = Variable::<f32, D>::new(x);
+        let w = Variable::<f32, D>::new(w);
+        let b = Variable::<f32, D>::new(b);
+
+        let y_hat = conv(x.clone(), w.clone(), Some(b.clone()), config);
+        assert_val_eq!(y_hat.clone(), y, 1e-4);
+        y_hat.backward();
+        assert_val_eq_grad!(x, dx, 1e-4);
+        assert_val_eq_grad!(w, dw, 1e-4);
+        assert_val_eq_grad!(b, db, 1e-4);
+    }
+    run_test!(conv_bias, conv_bias_cpu, conv_bias_gpu);
 }
--- zenu-autograd/src/nn/conv/mod.rs
+++ zenu-autograd/src/nn/conv/mod.rs
@@ -2,3 +2,4 @@ mod conv_with_bias;
 mod conv_without_bias;
 
 pub use conv_with_bias::conv;
+pub use conv_without_bias::ConvConfigs;
